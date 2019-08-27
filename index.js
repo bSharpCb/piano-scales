e3825b7913867616e6e23ecce2bfb4bc992be5f8
bSharpd <30420213+bSharpd@users.noreply.github.com>
2019-08-27 07:29:03 -0400

    Updating with additional functionality

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
 //optimization and new features wishlist 
-//combine makeColor functions into a single function that receives (element, color)
+
 
 var keys = [];
 		keys[0] = Array.from(document.getElementById('whitekeys').children);
 		keys[1] = Array.from(document.getElementById('blackkeys').children);
 
-		var naturalNotes = "cdefgab";
-		function elemForNote(noteName) {
-			var noteNum = naturalNotes.indexOf(noteName[0]);
-			var sharp = (noteName.length == 2) ? 1 : 0;
+var naturalNotes = "cdefgab";
 
-			return keys[sharp][noteNum];
-		}
+function elemForNote(noteName) {
+	var noteNum = naturalNotes.indexOf(noteName[0]);
+	var sharp = (noteName.length == 2) ? 1 : 0;
+	return keys[sharp][noteNum];
+}
 
-		function reset(elem) {
-				elem.style = "";
-		}
+function reset(elem) {
+		elem.style = "";
+}
+
+function makeColor(elem,colorString) {
+	elem.style = "background:" + colorString + ";";
+}
 		
-		function makeColor(elem,colorString) {
-			elem.style = "background:" + colorString + ";";
-		}
-		
 
 
-		//scale intervals
-		notes = ['c','c#','d','d#','e','f','f#','g','g#','a','a#','b'];
-		major = [2,2,1,2,2,2];
-		naturalMinor = [2,1,2,2,1,2];
-		harmonicMinor = [2,1,2,2,1,3];
-		melodicMinor = [2,1,2,2,2,2];
-		phrygianDominant = [1,3,1,2,1,2];
-		modeDorian = [2,1,2,2,2,1];
-		modePhyrigian = [1,2,2,2,1,2];
-		modeLydian = [2,2,2,1,2,2];
-		modeMixolydian = [2,2,1,2,2,1];
-		modeLocrian = [1,2,2,1,2,2];
+//scale intervals
+notes = ['c','c#','d','d#','e','f','f#','g','g#','a','a#','b'];
+major = [2,2,1,2,2,2];
+naturalMinor = [2,1,2,2,1,2];
+harmonicMinor = [2,1,2,2,1,3];
+melodicMinor = [2,1,2,2,2,2];
+phrygianDominant = [1,3,1,2,1,2];
+modeDorian = [2,1,2,2,2,1];
+modePhyrigian = [1,2,2,2,1,2];
+modeLydian = [2,2,2,1,2,2];
+modeMixolydian = [2,2,1,2,2,1];
+modeLocrian = [1,2,2,1,2,2];
 
-			function octaveAdjust(n){
-			if(n>11){
-				return notes[n-12];
-			}else{
-				return notes[n];
-			}
-		}
+//
 
+
+function octaveAdjust(n){
+	if(n>11){
+		return notes[n-12];
+	}else{
+		return notes[n];
+	}
+}
+/*to be reviewed and stuff
 		//chord structure
 		function minor7thChord(rootNoteId){
 			var note1 = notes.indexOf(rootNoteId);
@@ -64,51 +67,42 @@ var keys = [];
 			var fifthChord = minor7thChord(fifth);
 			return m7thChord, fourthChord, fifthChord; 
 		}
+*/
+function makeKeys(){
+	keys[0].forEach(reset);
+	keys[1].forEach(reset);
+}
 
-		function makeKeys(){
-			keys[0].forEach(reset);
-			keys[1].forEach(reset);
-		}
+makeKeys();
 
-		makeKeys();
+function intervals(key,intervalArray){
+	var rootNote = notes.indexOf(key);
+	var second = rootNote + intervalArray[0];
+	var third = second + intervalArray[1];
+	var fourth = third + intervalArray[2];
+	var fifth = fourth + intervalArray[3];
+	var sixth = fifth + intervalArray[4];
+	var seventh = sixth + intervalArray[5];
 
-		function octaveAdjust(n){
-			if(n>11){
-				return notes[n-12];
-			}else{
-				return notes[n];
-			}
-		}
-
-		function intervals(key,intervalArray){
-			var rootNote = notes.indexOf(key);
-			var second = rootNote + intervalArray[0];
-			var third = second + intervalArray[1];
-			var fourth = third + intervalArray[2];
-			var fifth = fourth + intervalArray[3];
-			var sixth = fifth + intervalArray[4];
-			var seventh = sixth + intervalArray[5];
-
-			myScale =[rootNote,second,third,fourth,fifth,sixth,seventh];
-			return myScale.map(octaveAdjust);
-	    }
+	myScale =[rootNote,second,third,fourth,fifth,sixth,seventh];
+	return myScale.map(octaveAdjust);
+}
 
 
 
-	    function blueNote(){
-		    	makeKeys();
-		    	var key = document.getElementById('keysig').value;
-		    	var sc_id = document.getElementById('scale').value;
-		    	var intervalArray = ['nothing',major,naturalMinor,harmonicMinor,melodicMinor,phrygianDominant, modeDorian, modePhyrigian, modeLydian, modeMixolydian, modeLocrian];
-		    	var scale = intervals(notes[key],intervalArray[sc_id]);
-		    	for(var m = 0; m < 7; m++){
-		    		makeColor(elemForNote(scale[m]),"blue");	    		
-		    	}
-		    	var rootN = document.getElementById('rootN');
-		    	rootN.value = key;
-		    	makeColor(elemForNote(scale[2]),"red");
-		    	makeColor(elemForNote(scale[6]),"green");
-		    	makeColor(elemForNote(scale[4]),"purple");
-		    	;
-	    }
+function blueNote(){
+    makeKeys();
+    var key = document.getElementById('keysig').value;
+    var sc_id = document.getElementById('scale').value;
+    var intervalArray = ['nothing',major,naturalMinor,harmonicMinor,melodicMinor,phrygianDominant, modeDorian, modePhyrigian, modeLydian, modeMixolydian, modeLocrian];
+    var scale = intervals(notes[key],intervalArray[sc_id]);
+	for(var m = 0; m < 7; m++){
+    	makeColor(elemForNote(scale[m]),"blue");	
+    	var target = document.getElementById('note' + m);
+		target.value = scale[m];    		
+	}
+	makeColor(elemForNote(scale[2]),"red");
+	makeColor(elemForNote(scale[6]),"green");
+	makeColor(elemForNote(scale[4]),"purple");
+}
 		    	
